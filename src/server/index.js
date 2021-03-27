87f20a74b474035fd9a4607b3d0127de8e9fbecf
m <mkostafa79@gmail.com>
2021-03-27 20:56:02 +0200

    m

--- src/server/index.js
+++ src/server/index.js
@@ -1,83 +1,22 @@
-// const mockAPIResponse = require('./mockAPI.js')
-
 const PORT = 8081;
-
-// const BASE_API_URL = 'https://api.meaningcloud.com/sentiment-2.1'
 const fetch = require("node-fetch");
 var bodyParser = require("body-parser");
 const app = require("./server");
-
-var cors = require("cors");
-app.use(cors());
-// var path = require('path')
-// require('dotenv').config()
-// let par = 'mostafa'
-
-// app.get('/', function(req,res){
-
-//     res.end("mostafa");
-// // })
-// path.resolve("src/client/index.js");
+// var cors = require("cors");
 
 app.get("/", function (req, res) {
-  // res.sendFile('dist/index.html')
-  //res.sendFile();
+
   res.json({ try: "it in front end serve" });
 });
-// INFO: a route that handling post request for new URL that coming from the frontend
-// app.post('/add-url', async (req, res) => {
-//     try {
-//         let body = req.body.url
-//         const SEND_URL = `${BASE_API_URL}?key=${process.env.API_KEY}&url=${body}&lang=en`
-//         let ss = fetch(SEND_URL, {
-//             method: 'post',
-//             body: '',
-//             headers: { 'Content-Type': 'application/json' }
-//         })
-//             .then((res) => res.json())
-//             .then((data) => {
-//                 let sen = data.sentence_list[1]
-//                 let sample = {
-//                     text: sen.text,
-//                     score_tag: sen.score_tag,
-//                     agreement: sen.agreement,
-//                     subjectivity: sen.subjectivity,
-//                     confidence: sen.confidence,
-//                     irony: sen.irony
-//                 }
-//                 console.error(sample)
-//                 res.json({ sample })
-//             })
-//     } catch (error) {
-//         console.log(error.message)
-//     }
-// })
 
-app.get("/test", function (req, res) {
-  res.send(mockAPIResponse);
-});
-
-module.exports = app;
-// designates what port the app will listen to for incoming requests
-app.listen(PORT, (error) => {
-  if (error) throw new Error(error);
-  console.log(`Server on port ${PORT}!`);
-});
-
-// TODO: export app to use it in the unit testing
-
-// app.listen(PORT, () => {
-//   console.log("Server has started!");
-// });
-
-const BASE_API_URL = "https://api.meaningcloud.com/sentiment-2.1";
+const BASE = "https://api.meaningcloud.com/sentiment-2.1";
 var bodyParser = require("body-parser");
 var path = require("path");
 require("dotenv").config();
 app.post("/add-url", async (req, res) => {
   try {
     let body = req.body.url;
-    const SEND_URL = `${BASE_API_URL}?key=${process.env.API_KEY}&url=${body}&lang=en`;
+    const SEND_URL = `${BASE}?key=${process.env.API_KEY}&url=${body}&lang=en`;
     let ss = fetch(SEND_URL, {
       method: "post",
       body: "",
@@ -86,18 +25,33 @@ app.post("/add-url", async (req, res) => {
       .then((res) => res.json())
       .then((data) => {
         let sen = data.sentence_list[1];
-        let sample = {
-          text: sen.text,
-          score_tag: sen.score_tag,
-          agreement: sen.agreement,
-          subjectivity: sen.subjectivity,
-          confidence: sen.confidence,
-          irony: sen.irony,
-        };
-        console.error(sample);
+       let sample = NLPData(sen);
         res.status(200).json({ sample });
       });
   } catch (error) {
     console.log(error.message);
   }
 });
+
+app.get("/test", function (req, res) {
+
+  res.status(200).json({ test: "scucess" });
+});
+
+
+function  NLPData(sen){
+              return {
+          text: sen.text,
+          score_tag: sen.score_tag,
+          agreement: sen.agreement,
+          subjectivity: sen.subjectivity,
+          confidence: sen.confidence,
+          irony: sen.irony,
+   }
+        }
+app.listen(PORT, (error) => {
+  if (error) throw new Error(error);
+  console.log(`Server on port ${PORT}!`);
+});
+
+
--- src/server/server.js
+++ src/server/server.js
@@ -1,15 +1,16 @@
 const express = require("express");
 const app = express();
+// app.use(cors());
 app.use(express.json());
 var path = require("path");
-app.use(express.static(path.join(__dirname, "client")));
+
+// app.use(express.static(path.join(__dirname, "client")));
 app.use(
   express.urlencoded({
     extended: true,
   })
 );
 
-// Middlewares...
-// Routes...
+
 
 module.exports = app;
