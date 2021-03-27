fe00ab65fafb0b32255e529fc628024fa8d6b35a
m <mkostafa79@gmail.com>
2021-03-28 01:00:19 +0200

    edit

--- src/server/server.js
+++ src/server/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const app = express();
-// app.use(cors());
+let cors = require('cors')
+
+app.use(cors());
 app.use(express.json());
 var path = require("path");
 
